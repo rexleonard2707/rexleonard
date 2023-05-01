4b8092a1f3fcb0a104c7e2684ee47703b3bfcc27
rexleonard2707 <125328968+rexleonard2707@users.noreply.github.com>
2023-05-02 04:12:46 +0700

    Add files via upload

---
+++ web-uts-kel/aboutdata.js
@@ -0,0 +1,10 @@
+let myparagraph = document.getElementById("paragraph1");
+let paragraphContent = myparagraph.textContent; 
+
+let img = document.getElementById("img").src;
+
+
+
+document.getElementById('paragraph1').innerHTML = namavariable;
+var paragraph1 = document.getElementById("paragraph1").innerHTML;
+
